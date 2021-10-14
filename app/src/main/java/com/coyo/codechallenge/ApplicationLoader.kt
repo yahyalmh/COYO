eea8735623ee61507822ca720a1f9b579dc08bc3
yahyalmh <yahyamh01@gmail.com>
2021-10-14 10:55:51 +0330

    Add Applicaiton class

---
+++ app/src/main/java/com/coyo/codechallenge/ApplicationLoader.kt
@@ -0,0 +1,12 @@
+package com.coyo.codechallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author yaya (@yahyalmh)
+ * @since 14th October 2021
+ */
+
+@HiltAndroidApp
+class ApplicationLoader : Application()
