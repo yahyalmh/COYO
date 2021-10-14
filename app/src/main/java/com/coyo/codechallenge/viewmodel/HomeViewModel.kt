1ed691693f0c7f7b7c189ab63af38cb0357604db
yahyalmh <yahyamh01@gmail.com>
2021-10-15 00:10:31 +0330

    Add viewmodel

---
+++ app/src/main/java/com/coyo/codechallenge/viewmodel/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package com.coyo.codechallenge.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.coyo.codechallenge.data.model.Post
+import com.coyo.codechallenge.data.repo.DbPostRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val dbPostRepository: DbPostRepository,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    private val clearListCh = Channel<Unit>(Channel.CONFLATED)
+
+    @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+    fun posts(): Flow<PagingData<Post>> {
+        return flowOf(
+            clearListCh.receiveAsFlow().map { PagingData.empty() },
+            dbPostRepository.fetchPostPage().cachedIn(viewModelScope)
+        ).flattenMerge(2)
+    }
+}
