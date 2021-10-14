51d0f8b9e36c763a0d7ad50b18a2fde38f946753
yahyalmh <yahyamh01@gmail.com>
2021-10-14 20:46:52 +0330

    Add api interface

---
+++ app/src/main/java/com/coyo/codechallenge/api/PlaceholderApi.kt
@@ -0,0 +1,42 @@
+package com.coyo.codechallenge.api
+
+import com.coyo.codechallenge.data.model.Post
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PlaceholderApi {
+    @GET("posts")
+    suspend fun getPosts(
+        @Query(pageNumberQueryParameterName) pageNumber: String,
+        @Query(pageSizeQueryParameterName) limit: String
+    ): List<Post>
+
+    companion object {
+        private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        private const val pageNumberQueryParameterName = "_page"
+        private const val pageSizeQueryParameterName = "_limit"
+
+        fun create(): PlaceholderApi {
+            val logger = HttpLoggingInterceptor().apply { level = Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL.toHttpUrlOrNull()!!)
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(PlaceholderApi::class.java)
+        }
+
+    }
+}
